c9547ef59f6a44a00be8b175791ed6626df7be6c
hombresenforma <ivan.hombresenforma@gmail.com>
2026-01-23 16:36:46 +0100

    Update juarui.js via Coach App Editor

---
+++ juarui.js
@@ -0,0 +1,21 @@
+// Titulo: P1_3D_fb_01
+const workoutData = {
+    "dia1": {
+        name: "Fullbody Push",
+        exercises: [
+            {
+                order: 1,
+                name: "Worm + Rotaciones de Tronco",
+                isSuperset: true,
+                items: [
+                    { name: "Worm + Rotaciones de Tronco", subOrder: 1, sets: 2, reps: "4", isSupersetStart: true, videoUrl: "https://www.youtube.com/watch?v=A8MFPgYNoGs", imageUrl: "https://i.ytimg.com/vi/A8MFPgYNoGs/mqdefault.jpg" },
+                    { name: "Sentadilla con Salto", subOrder: 2, sets: 2, reps: "10", notes: "Puedes hacerlas sin salto si lo prefieres.", rest: "30s", videoUrl: "https://www.youtube.com/watch?v=1-Mum4hyIJ0", imageUrl: "https://i.ytimg.com/vi/1-Mum4hyIJ0/mqdefault.jpg" }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=A8MFPgYNoGs",
+                imageUrl: "https://i.ytimg.com/vi/A8MFPgYNoGs/mqdefault.jpg"
+            },
+            { order: 2, name: "Press Banca en Multipower", sets: 3, reps: "8", rest: "90s", videoUrl: "https://youtu.be/w-5ovE5O5iU", imageUrl: "https://i.ytimg.com/vi/w-5ovE5O5iU/mqdefault.jpg" }
+        ]
+    }
+};
