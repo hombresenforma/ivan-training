33416cb87825a54ae3aa317b91572e8bb9a48610
hombresenforma <ivan.hombresenforma@gmail.com>
2026-02-02 19:25:40 +0100

    Update anibar.js via Coach App Editor

---
+++ anibar.js
@@ -0,0 +1,8 @@
+export const workoutData = {
+  "Día 1": {
+    name: "Torso",
+    exercises: [
+      { order: 1, name: "Press Banca", reps: "8-10", sets: 3, rest: "120s", imageUrl: "https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?w=400&h=400&fit=crop" }
+    ]
+  }
+};
