15a758795134b392876e49bdbae7086716bbaf7f
hombresenforma <ivan.hombresenforma@gmail.com>
2026-02-09 16:12:33 +0100

    Update javcua.js via Coach App Editor

--- javcua.js
+++ javcua.js
@@ -107,7 +107,7 @@ const workoutData = {
                 imageUrl: "https://i.ytimg.com/vi/r6iTJGOftHY/mqdefault.jpg"
             },
             {
-                order: 3,
+                order: 2,
                 name: "Movilidad - General",
                 sets: 1,
                 reps: "1",
@@ -118,43 +118,43 @@ const workoutData = {
             },
             {
                 order: 3,
-                name: "Activación - Dinamic Split 1",
+                name: "Activación - Push Up Walk - Caminada Sueca",
                 sets: 1,
                 reps: "10",
                 rest: "0",
-                notes: "10 repeticiones con cada pierna",
-                videoUrl: "https://youtu.be/ICFzvsVgOgU",
-                imageUrl: "https://i.ytimg.com/vi/ICFzvsVgOgU/mqdefault.jpg"
+                notes: "",
+                videoUrl: "https://youtu.be/xjgdNAK8l_A",
+                imageUrl: "https://i.ytimg.com/vi/xjgdNAK8l_A/mqdefault.jpg"
             },
             {
                 order: 4,
-                name: "Press Banca en Multipower (Explicado)",
-                sets: 3,
-                reps: "12",
-                rest: "60s",
-                notes: "Mira el vídeo donde explica los puntos más importantes para realizar bien la técnica del ejercicio\n\nSi no se pudiera realizar el ejercicio en la multipower, realiza flexiones (push up). Mismas series y repeticiones. Si no puedes hacerlas todas apoya la rodilla en el suelo y me apuntas en los comentarios cuantas has podido hacer sin y con rodillas",
-                videoUrl: "https://www.youtube.com/shorts/EDFhiF1mSKc",
-                imageUrl: "https://i.ytimg.com/vi/EDFhiF1mSKc/mqdefault.jpg"
-            },
-            {
-                order: 5,
-                name: "Jalón al Pecho en Polea",
+                name: "Jalón al Pecho Neutro en Polea",
                 sets: 3,
                 reps: "12",
                 rest: "60s",
                 notes: "La barra al bajar ha de ir al pecho. Cuando bajes los codos han de intentar estar cerca del tronco\n\nSi no se pudiera realizar el ejercicio de jalón al pecho, busca una barra horizontal y realiza el ejercicio alternativo. Misma técnica, barra al pecho y codos cerca del tronco al subir. Puedes graduar la intensidad usando las piernas. Si me cuesta mucho de espalda, usa piernas. Sé sincero e intenta trabajar bien la espalda, usando el mínimo las piernas. ",
-                videoUrl: "https://youtu.be/GYIhmy1P4vY",
-                imageUrl: "https://i.ytimg.com/vi/GYIhmy1P4vY/mqdefault.jpg"
+                videoUrl: "https://youtu.be/5YzMH2KkMHc",
+                imageUrl: "https://i.ytimg.com/vi/5YzMH2KkMHc/mqdefault.jpg"
             },
             {
-                order: 6,
-                name: "Sentadilla Goblet con Mancuerna/KTB (Explicado)",
+                order: 5,
+                name: "Press Banca Inclinado en Multipower",
                 sets: 3,
                 reps: "12",
                 rest: "60s",
-                notes: "En el vídeo se explica la técnica del ejercicio.\n\nSi no hubiera kettlebell disponible se puede hacer con mancuerna",
-                videoUrl: "https://www.youtube.com/shorts/tNu9bm3geqY",
-                imageUrl: "https://i.ytimg.com/vi/tNu9bm3geqY/mqdefault.jpg"
+                notes: "Misma técnica que el press banca plano. Recuerda 3 puntos de apoyo: Pies, glúteos y hombros\n\nSi no se pudiera realizar el ejercicio en la multipower, realiza flexiones (push up). Mismas series y repeticiones. Si no puedes hacerlas todas apoya la rodilla en el suelo y me apuntas en los comentarios cuantas has podido hacer sin y con rodillas",
+                videoUrl: "https://youtu.be/3GS7EjN7KSk",
+                imageUrl: "https://i.ytimg.com/vi/3GS7EjN7KSk/mqdefault.jpg"
+            },
+            {
+                order: 6,
+                name: "Carga Externa - Zancada",
+                sets: 3,
+                reps: "12",
+                rest: "60s",
+                notes: "Sacamos pecho y la bajada la realizaremos con el tronco vertical. Bajamos en 3 segundos y subo en 1. ",
+                videoUrl: "https://youtu.be/FqqcafS91zo",
+                imageUrl: "https://i.ytimg.com/vi/FqqcafS91zo/mqdefault.jpg"
             },
             {
                 order: 7,
@@ -167,18 +167,18 @@ const workoutData = {
                 },
                 items: [
                     {
-                        name: "CARDIO - Bicicleta (Ritmo Fuerte)",
+                        name: "Cardio - Remo en Máquina",
                         subOrder: 1,
                         sets: 1,
                         reps: "20s",
                         isCircuitItem: true,
-                        videoUrl: "https://www.youtube.com/watch?v=T4xdxoA4UzY",
-                        imageUrl: "https://i.ytimg.com/vi/T4xdxoA4UzY/mqdefault.jpg"
+                        videoUrl: "https://www.youtube.com/watch?v=kX7ccUa7Nwc",
+                        imageUrl: "https://i.ytimg.com/vi/kX7ccUa7Nwc/mqdefault.jpg"
                     }
                 ],
                 notes: "20 segundos de pedaleo a intensidad alta y 40 segundos de intensidad baja",
-                videoUrl: "https://www.youtube.com/watch?v=T4xdxoA4UzY",
-                imageUrl: "https://i.ytimg.com/vi/T4xdxoA4UzY/mqdefault.jpg"
+                videoUrl: "https://www.youtube.com/watch?v=kX7ccUa7Nwc",
+                imageUrl: "https://i.ytimg.com/vi/kX7ccUa7Nwc/mqdefault.jpg"
             }
         ]
     },
@@ -285,6 +285,15 @@ const exerciseAlternatives = {
         { name: "Dominadas Australianas con Pies en el Suelo", videoUrl: "https://www.youtube.com/watch?v=-GFyrw9BJVk", imageUrl: "https://i.ytimg.com/vi/-GFyrw9BJVk/mqdefault.jpg" }
     ],
     "Sentadilla Goblet con Mancuerna/KTB (Explicado)": [
+        { name: "", videoUrl: "", imageUrl: "" }
+    ],
+    "Jalón al Pecho Neutro en Polea": [
+        { name: "Dominadas Australianas con Pies en el Suelo", videoUrl: "https://www.youtube.com/watch?v=-GFyrw9BJVk", imageUrl: "https://i.ytimg.com/vi/-GFyrw9BJVk/mqdefault.jpg" }
+    ],
+    "Press Banca Inclinado en Multipower": [
+        { name: "Cuerpo Libre - Empuje - Push-Up", videoUrl: "https://youtu.be/Mq5JZLdYTaE", imageUrl: "https://i.ytimg.com/vi/Mq5JZLdYTaE/mqdefault.jpg" }
+    ],
+    "Carga Externa - Zancada": [
         { name: "", videoUrl: "", imageUrl: "" }
     ]
 };
