aeef0049aac0ba2590b4063a28389078f93da397
hombresenforma <ivan.hombresenforma@gmail.com>
2026-05-21 09:44:41 +0200

    Update eamdon.js via Coach App Editor

--- eamdon.js
+++ eamdon.js
@@ -1,10 +1,13 @@
-// Titulo: P4_3D_APTOR_HIIT_1
-// Notas: Estructura con pocos ejercicios y un HIIT 1:2 al final.
+// Titulo: P4_3D_APTOR_HIIT_2
+// Notas: - Añadimos 2 ejercicios en super-serie antes del HIIT.
+// - HIIT 1:1 y añadimos 2 vueltas más (6).
+// - En el día de Torso, añadimos 1 serie a los 2 básicos de brazos. 
+
 // Contenido de app_datos.js
+
 // =================================================================================
 // DATOS DE LA RUTINA (3 DÍAS)
 // =================================================================================
-
 const workoutData = {
     "dia1": {
         name: "Anterior HIIT",
@@ -13,7 +16,7 @@ const workoutData = {
                 order: 1,
                 name: "Flexiones con Peso Corporal",
                 sets: 2,
-                reps: "50",
+                reps: "100",
                 rest: "120s",
                 notes: "MÁXIMAS REPETICIONES (AL FALLO)",
                 videoUrl: "https://www.youtube.com/shorts/jqnnetMI-4s",
@@ -39,17 +42,9 @@ const workoutData = {
                         sets: 3,
                         reps: "24",
                         notes: "12 por lado.",
+                        rest: "90s",
                         videoUrl: "https://www.youtube.com/watch?v=eSAN1E5usJI",
                         imageUrl: "https://i.ytimg.com/vi/eSAN1E5usJI/mqdefault.jpg"
-                    },
-                    {
-                        name: "Plancha - Normal",
-                        subOrder: 3,
-                        sets: 3,
-                        reps: "20\"",
-                        rest: "90s",
-                        videoUrl: "https://youtu.be/DQ4wYExQRJo",
-                        imageUrl: "https://i.ytimg.com/vi/DQ4wYExQRJo/mqdefault.jpg"
                     }
                 ],
                 notes: "",
@@ -71,13 +66,13 @@ const workoutData = {
                         imageUrl: "https://i.ytimg.com/vi/iATqshmFPnI/mqdefault.jpg"
                     },
                     {
-                        name: "Fondos de Tríceps con Pies en el Suelo",
+                        name: "Fondos en Banco",
                         subOrder: 2,
                         sets: 3,
                         reps: "12",
                         rest: "90s",
-                        videoUrl: "https://youtube.com/shorts/7mpEJQC_gRk",
-                        imageUrl: "https://i.ytimg.com/vi/7mpEJQC_gRk/mqdefault.jpg"
+                        videoUrl: "https://youtu.be/NSB_HrDwd6I",
+                        imageUrl: "https://i.ytimg.com/vi/NSB_HrDwd6I/mqdefault.jpg"
                     }
                 ],
                 notes: "",
@@ -85,13 +80,41 @@ const workoutData = {
                 imageUrl: "https://i.ytimg.com/vi/iATqshmFPnI/mqdefault.jpg"
             },
             {
+                order: 4,
+                name: "Extensión de Cuádriceps en Máquina",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Extensión de Cuádriceps en Máquina",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "12",
+                        isSupersetStart: true,
+                        videoUrl: "https://www.youtube.com/watch?v=k1Nn0cJOMng",
+                        imageUrl: "https://i.ytimg.com/vi/k1Nn0cJOMng/mqdefault.jpg"
+                    },
+                    {
+                        name: "Elevaciones Laterales con Mancuernas",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "12",
+                        rest: "90s",
+                        videoUrl: "https://youtu.be/rhmW_fhB4cs",
+                        imageUrl: "https://i.ytimg.com/vi/rhmW_fhB4cs/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=k1Nn0cJOMng",
+                imageUrl: "https://i.ytimg.com/vi/k1Nn0cJOMng/mqdefault.jpg"
+            },
+            {
                 order: 5,
-                name: "HIIT 1:2",
+                name: "HIIT 1:1",
                 isSuperset: true,
                 circuitDetails: {
-                    totalRounds: 2,
-                    restBetweenExercisesSeconds: 60,
-                    restBetweenRoundsSeconds: 60
+                    totalRounds: 3,
+                    restBetweenExercisesSeconds: 30,
+                    restBetweenRoundsSeconds: 30
                 },
                 items: [
                     {
@@ -100,7 +123,6 @@ const workoutData = {
                         sets: 1,
                         reps: "30s",
                         isCircuitItem: true,
-                        notes: "Haz el descanso a ritmo suave, pero no pares.",
                         videoUrl: "https://youtu.be/KKvRtNn904g",
                         imageUrl: "https://i.ytimg.com/vi/KKvRtNn904g/mqdefault.jpg"
                     },
@@ -127,7 +149,7 @@ const workoutData = {
                 order: 1,
                 name: "Dominadas Asistidas en Máquina",
                 sets: 2,
-                reps: "50",
+                reps: "100",
                 rest: "120s",
                 notes: "Pon el 50% de tu peso corporal y haz las máximas repeticiones.",
                 videoUrl: "https://www.youtube.com/shorts/5OGBlINLP6w",
@@ -152,17 +174,9 @@ const workoutData = {
                         subOrder: 2,
                         sets: 3,
                         reps: "15",
-                        videoUrl: "https://www.youtube.com/watch?v=l6zEYjjJ4dE",
-                        imageUrl: "https://i.ytimg.com/vi/l6zEYjjJ4dE/mqdefault.jpg"
-                    },
-                    {
-                        name: "Sit Up",
-                        subOrder: 3,
-                        sets: 3,
-                        reps: "12",
                         rest: "90s",
-                        videoUrl: "https://youtu.be/fXgBSdee8QU",
-                        imageUrl: "https://i.ytimg.com/vi/fXgBSdee8QU/mqdefault.jpg"
+                        videoUrl: "https://www.youtube.com/watch?v=1-Mum4hyIJ0",
+                        imageUrl: "https://i.ytimg.com/vi/1-Mum4hyIJ0/mqdefault.jpg"
                     }
                 ],
                 notes: "",
@@ -198,13 +212,41 @@ const workoutData = {
                 imageUrl: "https://i.ytimg.com/vi/uH9Hg4nWOG8/mqdefault.jpg"
             },
             {
+                order: 4,
+                name: "Curl Femoral Tumbado en Máquina",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Curl Femoral Tumbado en Máquina",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "12",
+                        isSupersetStart: true,
+                        videoUrl: "https://www.youtube.com/shorts/-VfGwgG23OM",
+                        imageUrl: "https://i.ytimg.com/vi/-VfGwgG23OM/mqdefault.jpg"
+                    },
+                    {
+                        name: "Curl Martillo con Mancuernas",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "12",
+                        rest: "90s",
+                        videoUrl: "https://youtu.be/fcFsPoJY9lg",
+                        imageUrl: "https://i.ytimg.com/vi/fcFsPoJY9lg/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/shorts/-VfGwgG23OM",
+                imageUrl: "https://i.ytimg.com/vi/-VfGwgG23OM/mqdefault.jpg"
+            },
+            {
                 order: 5,
-                name: "HIIT 1:2",
+                name: "HIIT 1:1",
                 isSuperset: true,
                 circuitDetails: {
-                    totalRounds: 2,
-                    restBetweenExercisesSeconds: 60,
-                    restBetweenRoundsSeconds: 60
+                    totalRounds: 3,
+                    restBetweenExercisesSeconds: 30,
+                    restBetweenRoundsSeconds: 30
                 },
                 items: [
                     {
@@ -213,7 +255,6 @@ const workoutData = {
                         sets: 1,
                         reps: "30s",
                         isCircuitItem: true,
-                        notes: "Haz el descanso a ritmo suave, pero no pares.",
                         videoUrl: "https://www.youtube.com/watch?v=T4xdxoA4UzY",
                         imageUrl: "https://i.ytimg.com/vi/T4xdxoA4UzY/mqdefault.jpg"
                     },
@@ -239,7 +280,7 @@ const workoutData = {
             {
                 order: 1,
                 name: "Curl Scott con Barra Z",
-                sets: 3,
+                sets: 4,
                 reps: "8-10",
                 rest: "90s",
                 notes: "",
@@ -249,7 +290,7 @@ const workoutData = {
             {
                 order: 2,
                 name: "Fondos en Paralelas con Peso Corporal",
-                sets: 3,
+                sets: 4,
                 reps: "8-10",
                 rest: "90s",
                 notes: "Utiliza la máquina asistida o banda elástica si lo necesitas. ",
@@ -258,11 +299,11 @@ const workoutData = {
             },
             {
                 order: 3,
-                name: "Press Militar Sentado con Mancuernas (ExPLICADO)",
+                name: "Press Militar Sentado con Mancuernas (Explicado)",
                 isSuperset: true,
                 items: [
                     {
-                        name: "Press Militar Sentado con Mancuernas (ExPLICADO)",
+                        name: "Press Militar Sentado con Mancuernas (Explicado)",
                         subOrder: 1,
                         sets: 3,
                         reps: "12",
@@ -322,12 +363,12 @@ const workoutData = {
             },
             {
                 order: 5,
-                name: "HIIT 1:2",
+                name: "HIIT 1:1",
                 isSuperset: true,
                 circuitDetails: {
-                    totalRounds: 2,
-                    restBetweenExercisesSeconds: 60,
-                    restBetweenRoundsSeconds: 60
+                    totalRounds: 3,
+                    restBetweenExercisesSeconds: 30,
+                    restBetweenRoundsSeconds: 30
                 },
                 items: [
                     {
@@ -354,175 +395,19 @@ const workoutData = {
                 imageUrl: "https://i.ytimg.com/vi/kX7ccUa7Nwc/mqdefault.jpg"
             }
         ]
-    },
-    "dia4": {
-        name: " Fullbody barra 2",
-        exercises: [
-            {
-                order: 1,
-                name: "Movilidad - Cintura Escapular",
-                isSuperset: true,
-                items: [
-                    {
-                        name: "Movilidad - Cintura Escapular",
-                        subOrder: 1,
-                        sets: 1,
-                        reps: "8",
-                        isSupersetStart: true,
-                        notes: "Estabilizando el tronco con mucho control motor.",
-                        videoUrl: "https://youtu.be/qFt3RmTDmXI",
-                        imageUrl: "https://i.ytimg.com/vi/qFt3RmTDmXI/mqdefault.jpg"
-                    },
-                    {
-                        name: "Movilidad - Rodilla y Tobillo",
-                        subOrder: 2,
-                        sets: 1,
-                        reps: "8",
-                        videoUrl: "https://youtu.be/wdOGeCaNKS8",
-                        imageUrl: "https://i.ytimg.com/vi/wdOGeCaNKS8/mqdefault.jpg"
-                    },
-                    {
-                        name: "Movilidad - Cintura Pélvica",
-                        subOrder: 3,
-                        sets: 1,
-                        reps: "8",
-                        rest: "0",
-                        videoUrl: "https://youtu.be/zoEdWDF5EiA",
-                        imageUrl: "https://i.ytimg.com/vi/zoEdWDF5EiA/mqdefault.jpg"
-                    }
-                ],
-                notes: "Estabilizando el tronco con mucho control motor.",
-                videoUrl: "https://youtu.be/qFt3RmTDmXI",
-                imageUrl: "https://i.ytimg.com/vi/qFt3RmTDmXI/mqdefault.jpg"
-            },
-            {
-                order: 2,
-                name: "Press Militar con Barra de Pie",
-                isSuperset: true,
-                items: [
-                    {
-                        name: "Press Militar con Barra de Pie",
-                        subOrder: 1,
-                        sets: 4,
-                        reps: "10",
-                        isSupersetStart: true,
-                        videoUrl: "https://www.youtube.com/watch?v=idnuMZx6mS0",
-                        imageUrl: "https://i.ytimg.com/vi/idnuMZx6mS0/mqdefault.jpg"
-                    },
-                    {
-                        name: "Sentadilla Frontal con Barra",
-                        subOrder: 2,
-                        sets: 4,
-                        reps: "12",
-                        videoUrl: "https://youtu.be/ErXO1WKVlPI",
-                        imageUrl: "https://i.ytimg.com/vi/ErXO1WKVlPI/mqdefault.jpg"
-                    },
-                    {
-                        name: "Remo con Barra",
-                        subOrder: 3,
-                        sets: 4,
-                        reps: "12",
-                        rest: "90s",
-                        videoUrl: "https://youtu.be/MjnZ52mZgT0",
-                        imageUrl: "https://i.ytimg.com/vi/MjnZ52mZgT0/mqdefault.jpg"
-                    }
-                ],
-                notes: "",
-                videoUrl: "https://www.youtube.com/watch?v=idnuMZx6mS0",
-                imageUrl: "https://i.ytimg.com/vi/idnuMZx6mS0/mqdefault.jpg"
-            },
-            {
-                order: 5,
-                name: "Curl con Barra Recta",
-                isSuperset: true,
-                items: [
-                    {
-                        name: "Curl con Barra Recta",
-                        subOrder: 1,
-                        sets: 2,
-                        reps: "12",
-                        isSupersetStart: true,
-                        notes: "Puedes hacerlo de pie",
-                        videoUrl: "https://youtu.be/0TjnWWqQfUw",
-                        imageUrl: "https://i.ytimg.com/vi/0TjnWWqQfUw/mqdefault.jpg"
-                    },
-                    {
-                        name: "Flexiones / Push Ups",
-                        subOrder: 2,
-                        sets: 2,
-                        reps: "12",
-                        rest: "60s",
-                        videoUrl: "https://www.youtube.com/watch?v=GFUw7S0wDxk",
-                        imageUrl: "https://i.ytimg.com/vi/GFUw7S0wDxk/mqdefault.jpg"
-                    }
-                ],
-                notes: "Puedes hacerlo de pie",
-                videoUrl: "https://youtu.be/0TjnWWqQfUw",
-                imageUrl: "https://i.ytimg.com/vi/0TjnWWqQfUw/mqdefault.jpg"
-            },
-            {
-                order: 6,
-                name: "Circuito",
-                isSuperset: true,
-                circuitDetails: {
-                    totalRounds: 12,
-                    restBetweenExercisesSeconds: 10,
-                    restBetweenRoundsSeconds: 0
-                },
-                items: [
-                    {
-                        name: "Worm",
-                        subOrder: 1,
-                        sets: 1,
-                        reps: "20s",
-                        isCircuitItem: true,
-                        videoUrl: "https://www.youtube.com/watch?v=irBHRDg7ZHU",
-                        imageUrl: "https://i.ytimg.com/vi/irBHRDg7ZHU/mqdefault.jpg"
-                    },
-                    {
-                        name: "Burpee Completo",
-                        subOrder: 2,
-                        sets: 1,
-                        reps: "20s",
-                        isCircuitItem: true,
-                        videoUrl: "https://youtu.be/qfBhsTabWN0",
-                        imageUrl: "https://i.ytimg.com/vi/qfBhsTabWN0/mqdefault.jpg"
-                    },
-                    {
-                        name: "Zancada Dinámica",
-                        subOrder: 3,
-                        sets: 1,
-                        reps: "20s",
-                        isCircuitItem: true,
-                        videoUrl: "https://www.youtube.com/watch?v=eSAN1E5usJI",
-                        imageUrl: "https://i.ytimg.com/vi/eSAN1E5usJI/mqdefault.jpg"
-                    },
-                    {
-                        name: "Sit Up",
-                        subOrder: 4,
-                        sets: 1,
-                        reps: "20s",
-                        isCircuitItem: true,
-                        videoUrl: "https://youtu.be/fXgBSdee8QU",
-                        imageUrl: "https://i.ytimg.com/vi/fXgBSdee8QU/mqdefault.jpg"
-                    }
-                ],
-                notes: "",
-                videoUrl: "https://www.youtube.com/watch?v=irBHRDg7ZHU",
-                imageUrl: "https://i.ytimg.com/vi/irBHRDg7ZHU/mqdefault.jpg"
-            }
-        ]
     }
 };
 
 const exerciseAlternatives = {
     "Press Militar en Multipower": [
-        { name: "Press Militar Sentado con Mancuernas (Explicado)", videoUrl: "", imageUrl: "" }
+        { name: "Press Militar Sentado con Mancuernas (Explicado)", videoUrl: "https://www.youtube.com/shorts/2ZkYyh4ic0o", imageUrl: "https://i.ytimg.com/vi/2ZkYyh4ic0o/mqdefault.jpg" }
+    ],
+    "Curl Femoral Tumbado en Máquina": [
+        { name: "Curl Femoral Sentado en Máquina", videoUrl: "https://www.youtube.com/shorts/2fXW4I08ov4", imageUrl: "https://i.ytimg.com/vi/2fXW4I08ov4/mqdefault.jpg" }
     ],
     "Curl Scott con Barra Z": [
         { name: "Curl Araña con Barra Z", videoUrl: "https://www.youtube.com/shorts/ZGa2E8bi9Eg", imageUrl: "https://i.ytimg.com/vi/ZGa2E8bi9Eg/mqdefault.jpg" }
-    ],
-    "Press Militar con Barra de Pie": [
-        { name: "Press Militar Sentado con Mancuernas (Explicado)", videoUrl: "", imageUrl: "" }
     ]
 };
+
+console.log("Datos de rutina cargados desde el creador.");
