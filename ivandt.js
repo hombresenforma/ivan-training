977af2202595d82c6733f4fdf75d0d9954912f58
hombresenforma <ivan.hombresenforma@gmail.com>
2026-06-07 16:37:47 +0200

    Update ivandt.js via Coach App Editor

--- ivandt.js
+++ ivandt.js
@@ -1,8 +1,8 @@
-// Titulo: P19_TRISERIES
+// Titulo: P19_3D_PostAntTor_TRISERIES_1
 
 const workoutData = {
     "dia1": {
-        name: "Tracción",
+        name: "Posterior",
         exercises: [
             {
                 order: 1,
@@ -107,11 +107,242 @@ const workoutData = {
                 imageUrl: "https://i.ytimg.com/vi/-VfGwgG23OM/mqdefault.jpg"
             }
         ]
+    },
+    "dia2": {
+        name: "Anterior",
+        exercises: [
+            {
+                order: 1,
+                name: "Press Banca con Barra",
+                sets: 3,
+                reps: "6",
+                rest: "120s",
+                notes: "",
+                videoUrl: "https://youtu.be/PKpsrFS2uac",
+                imageUrl: "https://i.ytimg.com/vi/PKpsrFS2uac/mqdefault.jpg"
+            },
+            {
+                order: 2,
+                name: "Press de Pecho Sentado en Polea",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Press de Pecho Sentado en Polea",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        videoUrl: "https://youtu.be/sKFOgCQc-4o",
+                        imageUrl: "https://i.ytimg.com/vi/sKFOgCQc-4o/mqdefault.jpg"
+                    },
+                    {
+                        name: "Aperturas Sentado en Polea",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "12",
+                        videoUrl: "https://youtu.be/IaODfSloHRo",
+                        imageUrl: "https://i.ytimg.com/vi/IaODfSloHRo/mqdefault.jpg"
+                    },
+                    {
+                        name: "Flexiones con Peso Corporal",
+                        subOrder: 3,
+                        sets: 3,
+                        reps: "20",
+                        rest: "120s",
+                        videoUrl: "https://www.youtube.com/shorts/jqnnetMI-4s",
+                        imageUrl: "https://i.ytimg.com/vi/jqnnetMI-4s/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/sKFOgCQc-4o",
+                imageUrl: "https://i.ytimg.com/vi/sKFOgCQc-4o/mqdefault.jpg"
+            },
+            {
+                order: 3,
+                name: "Press Militar Sentado con Mancuernas (ExPLICADO)",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Press Militar Sentado con Mancuernas (ExPLICADO)",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        videoUrl: "https://www.youtube.com/shorts/2ZkYyh4ic0o",
+                        imageUrl: "https://i.ytimg.com/vi/2ZkYyh4ic0o/mqdefault.jpg"
+                    },
+                    {
+                        name: "Extensión de Tríceps en Polea con Cuerda",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "10",
+                        rest: "120s",
+                        videoUrl: "https://youtube.com/shorts/Eqi6CSuPbUQ",
+                        imageUrl: "https://i.ytimg.com/vi/Eqi6CSuPbUQ/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/shorts/2ZkYyh4ic0o",
+                imageUrl: "https://i.ytimg.com/vi/2ZkYyh4ic0o/mqdefault.jpg"
+            },
+            {
+                order: 4,
+                name: "Extensión de Cuádriceps en Máquina",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Extensión de Cuádriceps en Máquina",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "10",
+                        isSupersetStart: true,
+                        videoUrl: "https://www.youtube.com/watch?v=k1Nn0cJOMng",
+                        imageUrl: "https://i.ytimg.com/vi/k1Nn0cJOMng/mqdefault.jpg"
+                    },
+                    {
+                        name: "Extensión de Tríceps Unilat Trasnuca en Polea Alta",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "10",
+                        rest: "120s",
+                        videoUrl: "https://youtu.be/_2VPSbsjMQE",
+                        imageUrl: "https://i.ytimg.com/vi/_2VPSbsjMQE/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=k1Nn0cJOMng",
+                imageUrl: "https://i.ytimg.com/vi/k1Nn0cJOMng/mqdefault.jpg"
+            }
+        ]
+    },
+    "dia3": {
+        name: "Torso Completo",
+        exercises: [
+            {
+                order: 1,
+                name: "Press Inclinado con Mancuernas",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Press Inclinado con Mancuernas",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        videoUrl: "https://youtu.be/ZE4M73kXB5A",
+                        imageUrl: "https://i.ytimg.com/vi/ZE4M73kXB5A/mqdefault.jpg"
+                    },
+                    {
+                        name: "Fondos en Paralelas con Peso Corporal",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "15",
+                        rest: "120s",
+                        videoUrl: "https://youtube.com/shorts/om9U8WY5HoY",
+                        imageUrl: "https://i.ytimg.com/vi/om9U8WY5HoY/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/ZE4M73kXB5A",
+                imageUrl: "https://i.ytimg.com/vi/ZE4M73kXB5A/mqdefault.jpg"
+            },
+            {
+                order: 2,
+                name: "Jalón al Pecho en Polea",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Jalón al Pecho en Polea",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        notes: "Trata de despegar el pecho al final del movimiento",
+                        videoUrl: "https://youtu.be/GYIhmy1P4vY",
+                        imageUrl: "https://i.ytimg.com/vi/GYIhmy1P4vY/mqdefault.jpg"
+                    },
+                    {
+                        name: "Face Pull al Cuello en Polea Alta",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "15",
+                        rest: "120s",
+                        videoUrl: "https://www.youtube.com/shorts/TaTjLum-_qI",
+                        imageUrl: "https://i.ytimg.com/vi/TaTjLum-_qI/mqdefault.jpg"
+                    }
+                ],
+                notes: "Trata de despegar el pecho al final del movimiento",
+                videoUrl: "https://youtu.be/GYIhmy1P4vY",
+                imageUrl: "https://i.ytimg.com/vi/GYIhmy1P4vY/mqdefault.jpg"
+            },
+            {
+                order: 3,
+                name: "Curl Scott con Barra Z",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Curl Scott con Barra Z",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        notes: "Trata de despegar el pecho al final del movimiento",
+                        videoUrl: "https://www.youtube.com/watch?v=-Rzppjmt6ag",
+                        imageUrl: "https://i.ytimg.com/vi/-Rzppjmt6ag/mqdefault.jpg"
+                    },
+                    {
+                        name: "Curl con Barra Z Agarre Estrecho",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "15",
+                        rest: "120s",
+                        videoUrl: "https://youtu.be/NQZnaLU9kXs",
+                        imageUrl: "https://i.ytimg.com/vi/NQZnaLU9kXs/mqdefault.jpg"
+                    }
+                ],
+                notes: "Trata de despegar el pecho al final del movimiento",
+                videoUrl: "https://www.youtube.com/watch?v=-Rzppjmt6ag",
+                imageUrl: "https://i.ytimg.com/vi/-Rzppjmt6ag/mqdefault.jpg"
+            },
+            {
+                order: 4,
+                name: "Press Militar con Barra de Pie",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Press Militar con Barra de Pie",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "8",
+                        isSupersetStart: true,
+                        notes: "Trata de despegar el pecho al final del movimiento",
+                        videoUrl: "https://www.youtube.com/watch?v=idnuMZx6mS0",
+                        imageUrl: "https://i.ytimg.com/vi/idnuMZx6mS0/mqdefault.jpg"
+                    },
+                    {
+                        name: "Extensión de Tríceps Unilat en Polea Alta",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "15",
+                        rest: "120s",
+                        videoUrl: "https://youtu.be/oWLmfjBi6lM",
+                        imageUrl: "https://i.ytimg.com/vi/oWLmfjBi6lM/mqdefault.jpg"
+                    }
+                ],
+                notes: "Trata de despegar el pecho al final del movimiento",
+                videoUrl: "https://www.youtube.com/watch?v=idnuMZx6mS0",
+                imageUrl: "https://i.ytimg.com/vi/idnuMZx6mS0/mqdefault.jpg"
+            }
+        ]
     }
 };
 
 const exerciseAlternatives = {
     "Remo con Barra": [
         { name: "Peso Muerto Rumano con Barra", videoUrl: "https://youtu.be/R7FKam5GyNw", imageUrl: "https://i.ytimg.com/vi/R7FKam5GyNw/mqdefault.jpg" }
+    ],
+    "Press Banca con Barra": [
+        { name: "Sentadilla Anterior en Máquina Jaca", videoUrl: "https://youtube.com/shorts/saLWdiUe5eE", imageUrl: "https://i.ytimg.com/vi/saLWdiUe5eE/mqdefault.jpg" },
+        { name: "Sentadilla Trasera con Barra", videoUrl: "https://youtu.be/FK5XU_gaxAE", imageUrl: "https://i.ytimg.com/vi/FK5XU_gaxAE/mqdefault.jpg" }
     ]
 };
